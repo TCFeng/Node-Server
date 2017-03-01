45e986b76c4761ebe6e0d79922c074ef92f6dbfa
Felton Tung <felton_tung@wistron.com>
2017-03-01 18:46:54 +0800

    V3 disable websocket

--- scripts/serverPush.js
+++ scripts/serverPush.js
@@ -5,91 +5,107 @@ $(function () {
     var $table = $('#detHistoryTable');
     var tableIndex = 1;
     var data = []
-    $table.bootstrapTable({data: data});
+    $table.bootstrapTable({ data: data });
+
+    setInterval(function () {
+
+        $.ajax({
+            //url: "https://us-central1-polar-terminal-159303.cloudfunctions.net/helloHttp",
+            url: "http://" + location.host + "/getHistoryInfo",
+            type: "GET",
+            dataType: "json",
+            success: function (Jdata) {
+                $table.bootstrapTable('load', Jdata.result.textArray);
+                showImage(Jdata.result);
+            },
+            error: function () {
+                console.log("ERROR!!!");
+            }
+        })
+
+    }, 1000);
 
 
-    // if user is running mozilla then use it's built-in WebSocket
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-    // if browser doesn't support WebSocket, just show some notification and exit
-    if (!window.WebSocket) {
-        content.html($('<p>', {
-            text: 'Sorry, but your browser doesn\'t '
-            + 'support WebSockets.'
-        }));
-        input.hide();
-        $('span').hide();
-        return;
-    }
+    // // if user is running mozilla then use it's built-in WebSocket
+    // window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-    // open connection
-    var connection = new WebSocket('ws://'+location.host+'/webSocket');
+    // // if browser doesn't support WebSocket, just show some notification and exit
+    // if (!window.WebSocket) {
+    //     content.html($('<p>', {
+    //         text: 'Sorry, but your browser doesn\'t '
+    //         + 'support WebSockets.'
+    //     }));
+    //     input.hide();
+    //     $('span').hide();
+    //     return;
+    // }
 
-    connection.onopen = function () {
-        console.log("Connection Open...");
-    }
+    // // open connection
+    // var externalIp = '';
+    // var connection = new WebSocket('ws://'+'127.0.0.1:8080'+'/webSocket');
 
-    connection.onerror = function (error) {
-        console.log("Connection Error!");
-    }
+    // connection.onopen = function () {
+    //     console.log("Connection Open...");
+    // }
 
-    connection.onmessage = function (message) {
-        var now = new Date();
-        var imageJobj = JSON.parse(message.data);
-        var imageInfo = {
-            'seq': tableIndex++,
-            'date': now.toLocaleString(),
-            'sex': imageJobj.textPayload[0].faceAttributes.gender
-        }
+    // connection.onerror = function (error) {
+    //     console.log("Connection Error!");
+    // }
 
-
-        $table.bootstrapTable('prepend', imageInfo);
-        showImage(imageJobj);
-    };
+    // connection.onmessage = function (message) {
+    //     var now = new Date();
+    //     var imageJobj = JSON.parse(message.data);
+    //     var imageInfo = {
+    //         'seq': tableIndex++,
+    //         'date': now.toLocaleString(),
+    //         'sex': imageJobj.textPayload[0].faceAttributes.gender
+    //     }
 
 
-    setInterval(function() {
-        if (connection.readyState !== 1) {
-            console.log('Unable to comminucate with the WebSocket server.')
-        }
-    }, 3000);
+    //     $table.bootstrapTable('prepend', imageInfo);
+    //     showImage(imageJobj);
+    // };
+
+
+    // setInterval(function() {
+    //     if (connection.readyState !== 1) {
+    //         console.log('Unable to comminucate with the WebSocket server.')
+    //     }
+    // }, 3000);
 
     function showImage(imageJobj) {
 
-        var imageBase64 = imageJobj.raw_image
+        var imageBase64 = imageJobj.imageObj
 
         var canvas = document.getElementById("photoImage");
         var context = canvas.getContext("2d");
-        
+
         var image = new Image();
 
-        image.src = 'data:image/jpeg;base64,'+ imageBase64;
+        image.src = 'data:image/jpeg;base64,' + imageBase64;
 
-        var width = image.naturalWidth; 
-        var height = image.naturalHeight; 
-        
+        var width = image.naturalWidth;
+        var height = image.naturalHeight;
+
         canvas.width = width;
         canvas.height = height;
 
-        
+
         image.onload = function () {
             context.drawImage(image, 0, 0);
-            detectFace(imageJobj.textPayload)
+            detectFace(imageJobj.imageDetct)
         }
     }
 
     function detectFace(textPayload) {
-        var arrayLength = textPayload.length;
 
-        if (arrayLength > 0) {
+        if (textPayload != null) {
             var canvas = document.getElementById('photoImage');
             var context = canvas.getContext('2d');
             context.beginPath();
 
-            for (var i = 0; i < arrayLength; i++) {
-                var faceRectangle = textPayload[i].faceRectangle;
-                context.rect(faceRectangle.left, faceRectangle.top, faceRectangle.width, faceRectangle.height);
-            }
+            context.rect(textPayload.left, textPayload.top, textPayload.width, textPayload.height);
 
             context.lineWidth = 3;
             context.strokeStyle = 'red';
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
-const expressWs = require('express-ws')(express());
+//const expressWs = require('express-ws')(express());
 const path = require('path');
 const http = require('http');
+const request = require('request');
 const fs = require('fs');
 
-const port = process.env.PORT || 8080;
-const app = expressWs.app;
-const wss = expressWs.getWss('/webSocket');
-const bodyParser = require('body-parser')
 
+const port = process.env.PORT || 8080;
+const app = express();
+//const wss = expressWs.getWss('/webSocket');
+const bodyParser = require('body-parser');
+
+var tableIndex = 1;
+var data = {
+    textArray: [],
+    imageObj: '',
+    imageDetct: '',
+};
+
+function textState() {
+    this.seq = 0,
+        this.date = '',//now.toLocaleString(),
+        this.sex = ''//imageJobj.textPayload[0].faceAttributes.gender
+}
+
+//app.set('view engine', 'ejs');
 app.use("/scripts", express.static(__dirname + '/scripts'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,21 +37,39 @@ app.get('/showStatus', (req, res) => {
     res.sendFile(path.join(`${__dirname}/showStatus.html`));
 });
 
+app.get('/getHistoryInfo', (req, res) => {
+    res.json({
+        'result': data
+    });
+});
 
-
+app.get('/cleanHistoryInfo', (req, res) => {
+    data = {
+        textArray: [],
+        imageObj: '',
+        imageDetct: '',
+    }
+    res.json({
+        'result': 'OK'
+    });
+});
 
 app.post('/getServerPushInfo', (req, res) => {
-    wss.clients.forEach(function (client) {
-        client.send(JSON.stringify(req.body));
-    });
+
+    var dataObj = req.body;
+    var newTextState = new textState();
+    var now = new Date();
+    newTextState.seq = tableIndex++;
+    newTextState.date = now.toLocaleString();
+    newTextState.sex = dataObj.textPayload[0].faceAttributes.gender;
+    data.textArray.unshift(newTextState);
+    data.imageObj = dataObj.raw_image;
+    data.imageDetct = dataObj.textPayload[0].faceRectangle;
     res.json({
         'msg': 'Success!!'
     });
 });
 
-app.ws('/webSocket', function (ws, req) {
-    console.log('socket', '...');
-});
 
 //Temp Test
 app.post('/getImageInfo', (req, res) => {
@@ -53,7 +87,7 @@ app.post('/getImageInfo', (req, res) => {
         });
         req.on('end', function () {
             var buf = new Buffer(data, 'binary');
-             res.json({
+            res.json({
                 'msg': buf.toString('base64')
             });
         });
@@ -61,8 +95,42 @@ app.post('/getImageInfo', (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`app is listening at port ${port}`);
+
+http.createServer(app).listen(port, () => {
+    console.log(`App listening on port ${port}`);
+    console.log('Press Ctrl+C to quit.');
 });
 
+
+
+// app.ws('/webSocket', function (ws, req) {
+//     console.log('socket', '...');
+// });
+
+
+// const wsServer = app.listen('65080', () => {
+//     console.log('Websocket server listening on port %s', wsServer.address().port);
+// });
+
+
+// const METADATA_NETWORK_INTERFACE_URL = 'http://metadata/computeMetadata/v1/' +
+//     '/instance/network-interfaces/0/access-configs/0/external-ip';
+
+// function getExternalIp(cb) {
+//     const options = {
+//         url: METADATA_NETWORK_INTERFACE_URL,
+//         headers: {
+//             'Metadata-Flavor': 'Google'
+//         }
+//     };
+
+//     request(options, (err, resp, body) => {
+//         if (err || resp.statusCode !== 200) {
+//             console.log('Error while talking to metadata server, assuming localhost');
+//             cb('localhost');
+//             return;
+//         }
+//         cb(body);
+//     });
+// }
 
